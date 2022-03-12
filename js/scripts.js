0f70183b02be0b5b523636f923f19c6b9e453c22
Robert Kirui <kirugik79@gmail.com>
2022-03-12 22:35:21 +0300

    Implement show and hide jQuery methods to toggle the description of the development and product management icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
 $(document).ready(function () {
+    // design section
     $("#design-icon").on("click", function () {
         $("#design-para").show();
+        $("#design-icon").hide(); 
     $("#design-para").on("click", function () {
-        $("#design-icon").hide();
+        $("#design-icon").show();
+        $("#design-para").hide(); 
+        });
+    });
+
+    // development section
+    $("#development-icon").on("click", function () {
+        $("#development-para").show();
+        $("#development-icon").hide(); 
+    $("#development-para").on("click", function () {
+        $("#development-icon").show();
+        $("#development-para").hide(); 
+        });
+    });
+
+    // product management section
+    $("#prod-icon").on("click", function () {
+        $("#prod-para").show();
+        $("#prod-icon").hide(); 
+    $("#prod-para").on("click", function () {
+        $("#prod-icon").show();
+        $("#prod-para").hide(); 
         });
     });
 });
