06dd455cf9273af844a568f804d3229fb9ab33b5
Robert Kirui <kirugik79@gmail.com>
2022-03-14 10:22:16 +0300

    Make portfolio section responsive

--- js/scripts.js
+++ js/scripts.js
@@ -35,88 +35,96 @@ $(document).ready(function () {
 //portfolio project 1
 $(document).ready(function () {
     $("#project1").mouseover(function(){
-        $("#project1").fadeTo('slow', 0.2);
+        $("#project1").fadeTo("slow", 0.3);
         $("#project-one").show();
       });
     $("#project1").mouseout(function(){
         $("#project-one").hide();
+        $("#project1").fadeTo("slow", 1);
     });
 });
 
 //portfolio project 2
 $(document).ready(function () {
     $("#project2").mouseover(function(){
-        $("#project2").fadeTo('slow', 0.2);
+        $("#project2").fadeTo("slow", 0.3);
         $("#project-two").show();
       });
     $("#project2").mouseout(function(){
         $("#project-two").hide();
+        $("#project2").fadeTo("slow", 1);
     });
 });
 
 //portfolio project 3
 $(document).ready(function () {
     $("#project3").mouseover(function(){
-        $("#project3").fadeTo('slow', 0.2);
+        $("#project3").fadeTo("slow", 0.3);
         $("#project-three").show();
       });
     $("#project3").mouseout(function(){
         $("#project-three").hide();
+        $("#project3").fadeTo("slow", 1);
     });
 });
 
 //portfolio project 4
 $(document).ready(function () {
     $("#project4").mouseover(function(){
-        $("#project4").fadeTo('slow', 0.2);
+        $("#project4").fadeTo("slow", 0.3);
         $("#project-four").show();
       });
     $("#project4").mouseout(function(){
         $("#project-four").hide();
+        $("#project4").fadeTo("slow", 1);
     });
 });
 
 //portfolio project 5
 $(document).ready(function () {
     $("#project5").mouseover(function(){
-        $("#project5").fadeTo('slow', 0.2);
+        $("#project5").fadeTo("slow", 0.3);
         $("#project-five").show();
       });
     $("#project5").mouseout(function(){
         $("#project-five").hide();
+        $("#project5").fadeTo("slow", 1);
     });
 });
 
 //portfolio project 6
 $(document).ready(function () {
     $("#project6").mouseover(function(){
-        $("#project6").fadeTo('slow', 0.2);
+        $("#project6").fadeTo("slow", 0.3);
         $("#project-six").show();
       });
     $("#project6").mouseout(function(){
         $("#project-six").hide();
+        $("#project6").fadeTo("slow", 1);
     });
 });
 
 //portfolio project 7
 $(document).ready(function () {
     $("#project7").mouseover(function(){
-        $("#project7").fadeTo('slow', 0.2);
+        $("#project7").fadeTo("slow", 0.3);
         $("#project-seven").show();
       });
     $("#project7").mouseout(function(){
         $("#project-seven").hide();
+        $("#project7").fadeTo("slow", 1);
     });
 });
 
 //portfolio project 8
 $(document).ready(function () {
     $("#project8").mouseover(function(){
-        $("#project8").fadeTo('slow', 0.2);
+        $("#project8").fadeTo("slow", 0.3);
         $("#project-eight").show();
       });
     $("#project8").mouseout(function(){
         $("#project-eight").hide();
+        $("#project8").fadeTo("slow", 1);
     });
 });
 
